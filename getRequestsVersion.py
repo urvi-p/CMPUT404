d4546ee80ca9b3ec8b747b26e6275c9d032264be
Urvi Patel <upatel@ualberta.ca>
2022-09-14 17:48:21 -0600

    download and print source code

--- getRequestsVersion.py
+++ getRequestsVersion.py
@@ -1,7 +1,14 @@
 import requests
 
+# Virtualenv: Question 4
 # print(requests.__version__)
 
-res = requests.get("http://www.google.com/")
+# Curl: Question 5
+# res = requests.get("http://www.google.com/")
+# print(res.text)
 
+# Curl: Question 10
+rawURL = "https://raw.githubusercontent.com/urvi-p/CMPUT404-Labs/main/getRequestsVersion.py"
+
+res = requests.get(rawURL)
 print(res.text)
